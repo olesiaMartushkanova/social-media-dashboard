89d2a8cfc8914b9a63c7cdb6142a2773a14c5120
olesiaMartushkanova <olesia.martushkanova@seeeverything.com>
2021-10-03 20:42:55 +1300

    built a sceleton

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div>
+        <div>Social Media Dashboard</div>
+        <div>Total Followers: 23,004</div>
+      </div>
+
+      <div>Social tiles here</div>
+      <div>Overview</div>
+      <div>Social sub-tiles here</div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'core-js/features/array/flat-map';
 import 'core-js/features/map';
 import 'core-js/features/promise';
 import 'core-js/features/set';
 import 'raf/polyfill';
 import 'whatwg-fetch';
+import { App } from './App';
 
-ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('app-root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app-root')
+);
