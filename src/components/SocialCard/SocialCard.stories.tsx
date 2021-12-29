84af7858a57ff399d9971865e17a21e1b0a97ecb
olesiaMartushkanova <olesia.martushkanova@seeeverything.com>
2021-12-29 20:39:00 +1300

    working on first story

--- src/components/SocialCard/SocialCard.stories.tsx
+++ src/components/SocialCard/SocialCard.stories.tsx
@@ -3,42 +3,31 @@ import { Story, Meta } from '@storybook/react';
 import SocialCard, { ISocialCard } from './SocialCard';
 import IconFacebook from '../../images/icon-facebook.svg';
 
-const Template: Story<ISocialCard> = (props) => {
-  const cards: ISocialCard[] = [
-    {
-      id: 'facebook-social-card',
-      iconTitle: '@nathanf',
-      interactions: 1987,
-      todayStatistic: 12,
-      icon: IconFacebook,
-      isFollowers: true,
-      topLineColor: 'hsl(208, 92%, 53%)',
-      isIncreasedActivity: true,
-    },
-  ];
+const Template: Story<ISocialCard> = ({
+  isFollowers,
+  isIncreasedActivity,
+  topLineColor,
+  icon,
+}: ISocialCard) => (
+  <SocialCard
+    id={'test-social-card'}
+    iconTitle={'@nathanf'}
+    interactions={1987}
+    todayStatistic={12}
+    icon={icon}
+    topLineColor={topLineColor}
+    isFollowers={isFollowers}
+    isIncreasedActivity={isIncreasedActivity}
+  />
+);
 
-  return (
-    <div>
-      {cards.map((card) => (
-        <li key={card.id}>
-          <SocialCard
-            id={card.id}
-            iconTitle={card.iconTitle}
-            interactions={card.interactions}
-            todayStatistic={card.todayStatistic}
-            icon={card.icon}
-            topLineColor={card.topLineColor}
-            isFollowers={card.isFollowers}
-            isIncreasedActivity={card.isIncreasedActivity}
-          />
-        </li>
-      ))}
-    </div>
-  );
-};
-
-export const Default = Template.bind({});
-Default.args = {} as ISocialCard;
+export const FacebookCard = Template.bind({});
+FacebookCard.args = {
+  icon: IconFacebook,
+  isFollowers: true,
+  topLineColor: 'hsl(208, 92%, 53%)',
+  isIncreasedActivity: true,
+} as ISocialCard;
 
 export default {
   title: 'SocialCard',
